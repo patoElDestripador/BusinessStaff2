989e2d753a13195984a00c34ddb1afb0b0e18415
Andres <142694258+patoElDestripador@users.noreply.github.com>
2024-04-15 19:15:19 -0500

    oli de nuevo2

---
+++ Models/CheckInCheckOut.cs
@@ -0,0 +1,9 @@
+namespace businessStaff2.Models;
+
+public class User
+{
+    public int ID { get; set; }
+    public int IdUser { get; set; }
+    public DATETIME EntryHour { get; set; }
+    public DATETIME DepartureHour { get; set; }
+}
---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+namespace businessStaff2.Models;
+
+public class User
+{
+    public int ID { get; set; }
+    public int IdEmployee { get; set; }
+    public string? Password { get; set; }
+    public string? UserName { get; set; }
+    public string? status { get; set; }
+    public string? Rol { get; set; }
+    public int ID { get; set; }
+}
